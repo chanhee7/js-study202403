82eb049bcda00ad6b522604f1c84abb76958e14d
chan <chan9505@naver.com>
2024-03-24 22:23:32 +0900

    # 자바스크립트 기초: 변수, 데이터타입, 연산자

---
+++ 1. core javascript/1. 자바스크립트 기초/1. 변수.js
@@ -0,0 +1,46 @@
+
+var result; //변수 선언
+
+result = 10 + 20; // 값을 변수에 할당(대입)
+
+var multiple = result * 3; // 90
+console.log(multiple);
+
+var fruit;
+fruit = '자몽'; 
+console.log(fruit);
+
+var food = '볶음밥';
+food = '돈까스';
+console.log(food);
+
+
+// 변수 이름 규칙
+// var 7number; // (X)
+var num7ber;   // (O)
+
+// var user name; // (X)
+var current_login_user_phone_number; // snake case
+var currentLoginUserPhoneNumber; // camel case -> 자바스크립트에 사용하기
+
+// 밑에 세가지는 각각 다른변수 (대소문자 구분)
+var apple;
+var APPLE;
+var Apple;
+
+// var myPetName!; // (X)
+var $myPetName_; // (O)
+
+// var for; // (X)
+// var let; // (X)
+var For; // (O)
+var lEt; // (O)
+
+// 변수명은 영어로쓰기
+var 젤리 = '곰표젤리';
+console.log(젤리);
+
+const $orange ='#ffa808';
+// $orange = 'sdfasdf';
+console.log($orange);
+
