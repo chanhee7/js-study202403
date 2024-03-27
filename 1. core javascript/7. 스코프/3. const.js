63f35ca848a7db5971ed4316c07682404361c077
chan <chan9505@naver.com>
2024-03-27 22:14:34 +0900

    # 함수 응용

---
+++ 1. core javascript/7. 스코프/3. const.js
@@ -0,0 +1,30 @@
+
+let x;
+console.log(x);
+
+// const : 상수 선언
+const y = 'ㅇㅇ';
+console.log(y);
+
+const PI = 3.14159265;
+const COLOR_GREEN = '#0f0';
+
+/*
+  $div.style.background = COLOR_GREEN;
+*/
+
+// 상수는 객체의 불변성을 유지할 수 있다.
+const kim = {
+  name: '김철수',
+  age: 30
+};
+kim.age = 31;
+kim.name = '마이클 킴';
+
+const array = {
+  0: 'z',
+  1: 'dd',
+  2: 'ddf',
+  length: 3
+}
+array.length
