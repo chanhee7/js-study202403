cb4838bf8080f0ac1b50515392eb6c45a82b57c7
chanhee7 <chan9505@naver.com>
2024-04-03 18:18:45 +0900

    # 계산기

---
+++ 2. web-api/9. CALCULATOR/js/app.js
@@ -0,0 +1,17 @@
+
+// 실행코드 (함수 호출, 이벤트등록..., 핸들러정의)
+
+// 이벤트 핸들러 바인딩
+$addBtn.addEventListener('click', e => {
+  // 연산 처리 함수
+  caculate(e);
+});
+$subtractBtn.addEventListener('click', e => {
+  caculate(e);
+});
+$multiplyBtn.addEventListener('click', e => {
+  caculate(e);
+});
+$divideBtn.addEventListener('click', e => {
+  caculate(e);
+});
