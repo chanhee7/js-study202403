8f542bbfadee64af7ad991978a0fc04a8237ba2e
chan <chan9505@naver.com>
2024-06-11 11:18:45 +0900

    # 실전예제: 동적 렌더링

---
+++ 4.OOP/js/components/Product.js
@@ -0,0 +1,12 @@
+
+class Product {
+  constructor(title, price, desc, image) {
+    this.title = title;
+    this.price = price;
+    this.desc = desc;
+    this.image = image;
+  }
+}
+
+// export default 는 한 모듈에 하나만 있어야 함
+export default Product;
